da7516b0499ac4ef116dde5ce082325454e537eb
Nikita-pyth <siorito321@gmail.com>
2025-04-22 16:34:45 +0300

    ahhh

--- car/serializers.py
+++ car/serializers.py
@@ -4,13 +4,13 @@ from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)  # ← required for matching expected output
+    id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(required=False, allow_blank=True)
+    problem_description = serializers.CharField(required=False,
+                                                allow_blank=True)
 
     def create(self, validated_data) -> Car:
         return Car(**validated_data)
-
