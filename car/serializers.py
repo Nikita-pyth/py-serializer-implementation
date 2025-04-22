d683960508cee4395184bd90632527fe678f2f04
Nikita-pyth <siorito321@gmail.com>
2025-04-22 16:22:23 +0300

    Solution improved

--- car/serializers.py
+++ car/serializers.py
@@ -11,12 +11,3 @@ class CarSerializer(serializers.Serializer):
                                             default=None)
     is_broken = serializers.BooleanField(default=False)
     problem_description = serializers.CharField(required=False)
-
-    def serialize_car_object(self, car: Car) -> dict:
-        serializer = CarSerializer(instance=car)
-        return serializer.data
-
-    def deserialize_car_object(self, json_data: dict) -> Car:
-        serializer = CarSerializer(data=json_data)
-        serializer.is_valid(raise_exception=True)
-        return Car(**serializer.validated_data)
