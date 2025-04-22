7ad0c85aaa18c11274d747917946e8b4ae33d9f1
Nikita-pyth <siorito321@gmail.com>
2025-04-22 16:37:40 +0300

    asfalt

--- car/serializers.py
+++ car/serializers.py
@@ -4,7 +4,7 @@ from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+    id = serializers.IntegerField(read_only=False)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
