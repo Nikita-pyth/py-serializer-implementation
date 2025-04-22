99f35f2e38e0fe70d15160d47a3b2131c42ed639
Nikita-pyth <siorito321@gmail.com>
2025-04-22 16:39:22 +0300

    asfalt

--- car/serializers.py
+++ car/serializers.py
@@ -9,8 +9,11 @@ class CarSerializer(serializers.Serializer):
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(required=False,
-                                                allow_blank=True)
+    problem_description = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        default=""
+    )
 
     def create(self, validated_data) -> Car:
         return Car(**validated_data)
