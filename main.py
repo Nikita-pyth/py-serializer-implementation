e38d9c1aab117a49c7efc56ff84660879e770d1b
Nikita-pyth <siorito321@gmail.com>
2025-04-22 16:26:10 +0300

    huh

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from car.models import Car
+from car.serializers import CarSerializer
 
 
-def serialize_car_object(car: Car) -> bytes:
-    pass
+def serialize_car_object(car: Car) -> dict:
+    serializer = CarSerializer(instance=car)
+    return serializer.data
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json_data: dict) -> Car:
+    serializer = CarSerializer(data=json_data)
+    serializer.is_valid(raise_exception=True)
+    return Car(**serializer.validated_data)
