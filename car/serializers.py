04dee480603deb54f361308580124e15d9d031ac
Nikita-pyth <siorito321@gmail.com>
2025-04-22 16:23:44 +0300

    flake8 addressed

--- car/serializers.py
+++ car/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 
-from car.models import Car
-
 
 class CarSerializer(serializers.Serializer):
     manufacturer = serializers.CharField(max_length=64, default="")
